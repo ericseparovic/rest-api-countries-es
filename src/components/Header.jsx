5f8c2d710369827c4e24a7806203ef5bfc035171
Eric Separovic <ericseparovic07@gmail.com>
2023-03-15 15:53:21 -0300

    Add icon moon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
 function Header() {
+	const element = <FontAwesomeIcon icon={faMoon} />;
 	return (
 		<header className='border-b-2'>
 			<div className='flex justify-between items-center h-20 px-4'>
 				<h1 className='font-bold'>Where in the word?</h1>
-				<button className='text-gray-800'>Dark Mode</button>
+
+				<div className='flex item-center gap-2'>
+					<div className='w-5'>{element}</div>
+					<button className='text-gray-800'>Dark Mode</button>
+				</div>
 			</div>
 		</header>
 	);

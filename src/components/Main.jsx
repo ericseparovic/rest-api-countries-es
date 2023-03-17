2ad6c88dba4254cfd778ba8da925bbd1247c395a
Eric Separovic <ericseparovic07@gmail.com>
2023-03-17 11:40:34 -0300

    Create search component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import Search from './Search';
 
 function Main() {
 	return (
 		<main className='bg-gray-100 h-screen flex flex-col items-center p-8'>
 			<form className='w-full'>
-				<div className='mb-12 relative'>
-					<div className='absolute flex items-center p-5'>
-						<FontAwesomeIcon icon={faMagnifyingGlass} />
-					</div>
-					<input
-						className='p-4 pl-16 rounded-md w-full'
-						type='search'
-						name=''
-						id=''
-						placeholder='Search for a country...'
-					/>
-				</div>
+				<Search />
 			</form>
 			<section>
 				<div>Card</div>
---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+function Search() {
+	return (
+		<div className='mb-12 relative'>
+			<div className='absolute flex items-center p-5'>
+				<FontAwesomeIcon icon={faMagnifyingGlass} />
+			</div>
+			<input
+				className='p-4 pl-16 rounded-md w-full'
+				type='search'
+				name=''
+				id=''
+				placeholder='Search for a country...'
+			/>
+		</div>
+	);
+}
+
+export default Search;

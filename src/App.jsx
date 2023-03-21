28d9e7281848a83fe69a2ce7003b9396f235a9ab
Eric Separovic <ericseparovic07@gmail.com>
2023-03-21 12:17:59 -0300

    Change path useAxios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import { useAxios } from './Hooks/useAxios';
 
 function App() {
-	const URL = 'src/data.json';
-	const { data, isLoading, hasError } = useAxios(URL);
-
 	return (
 		<div>
 			<Header />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
 import Search from './Search';
 import Filter from './Filter';
+import { useAxios } from '../Hooks/useAxios';
 
 function Main() {
+	const URL = 'src/data.json';
+	const { data, isLoading, hasError } = useAxios(URL);
+
 	return (
 		<main className='bg-gray-100 h-screen flex flex-col items-center p-8'>
 			<form className='w-full md:flex justify-between'>

b3c969c692fd737698a3c9f129088b91cf1d0a12
Eric Separovic <ericseparovic07@gmail.com>
2023-03-21 12:16:16 -0300

    Create hooks useAxios for get data json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import { useAxios } from './Hooks/useAxios';
 
 function App() {
+	const URL = 'src/data.json';
+	const { data, isLoading, hasError } = useAxios(URL);
+
 	return (
 		<div>
 			<Header />

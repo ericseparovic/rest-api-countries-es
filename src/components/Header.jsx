de6135eedc53a045a23d17b20c15445b27809872
Eric Separovic <ericseparovic07@gmail.com>
2023-03-22 10:28:25 -0300

    Add icon light

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon as faMoonLight } from '@fortawesome/free-regular-svg-icons';
+import { faMoon as faMoonDark } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
-	const element = <FontAwesomeIcon icon={faMoon} />;
+	const iconFaMoonLight = <FontAwesomeIcon icon={faMoonLight} size='lg' />;
+	const iconFaMoonDark = <FontAwesomeIcon icon={faMoonDark} size='lg' />;
+
 	return (
 		<header className='container mx-auto px-5'>
 			<div className='w-full flex justify-between h-20 items-center'>
-				<h1 className='font-bold'>Where in the word?</h1>
-
+				<h1 className='font-bold text-xl'>Where in the word?</h1>
 				<div className='flex item-center gap-2'>
-					<div className='w-5'>{element}</div>
+					<div>{iconFaMoonLight}</div>
 					<button className='text-gray-800'>Dark Mode</button>
 				</div>
 			</div>

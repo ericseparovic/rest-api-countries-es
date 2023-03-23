69aa90b5a7c4030a7b1090ba5a66effe491af284
Eric Separovic <ericseparovic07@gmail.com>
2023-03-23 12:03:56 -0300

    create routes

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,30 +1,34 @@
+import { Link } from 'react-router-dom';
+
 function Country({ country }) {
 	const { name, population, region, capital, flag } = country;
 
 	return (
-		<div className='rounded-md overflow-hidden max-w-xs shadow-lg'>
-			<div>
-				<img
-					className='w-full object-cover h-56'
-					src={flag}
-					alt='img country'
-				/>
-			</div>
-			<div className='bg-white p-6'>
-				<h2 className='font-bold text-xl mb-4'>{name}</h2>
-				<p className='text-sm pb-2 text-gray-800 font-medium'>
-					Population:{' '}
-					<span className='text-gray-700 font-light'>{population}</span>
-				</p>
-				<p className='text-sm pb-2 text-gray-800 font-medium'>
-					Region: <span className='text-gray-700 font-light'>{region}</span>
-				</p>
+		<Link to={'country-detail'}>
+			<div className='rounded-md overflow-hidden max-w-xs shadow-lg'>
+				<div>
+					<img
+						className='w-full object-cover h-56'
+						src={flag}
+						alt='img country'
+					/>
+				</div>
+				<div className='bg-white p-6'>
+					<h2 className='font-bold text-xl mb-4'>{name}</h2>
+					<p className='text-sm pb-2 text-gray-800 font-medium'>
+						Population:{' '}
+						<span className='text-gray-700 font-light'>{population}</span>
+					</p>
+					<p className='text-sm pb-2 text-gray-800 font-medium'>
+						Region: <span className='text-gray-700 font-light'>{region}</span>
+					</p>
 
-				<p className='text-sm pb-2 text-gray-800 font-medium'>
-					Capital: <span className='text-gray-700 font-light'>{capital}</span>
-				</p>
+					<p className='text-sm pb-2 text-gray-800 font-medium'>
+						Capital: <span className='text-gray-700 font-light'>{capital}</span>
+					</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import ReactDOM from 'react-dom/client';
 import React from 'react';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Routes/Home';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: 'country-detail',
+		element: <h1>Country Detail</h1>,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );

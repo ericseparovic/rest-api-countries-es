6c035b4ee2ac706e39a782a6fddfc0061227eede
Eric Separovic <ericseparovic07@gmail.com>
2023-03-27 08:50:10 -0300

    Show data in Detail component

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -17,10 +17,9 @@ function Detail() {
 
 	const [country, setCountry] = useState({});
 	const [error, setError] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
 	async function getCountry(numericCode) {
-		setIsLoading(true);
 		const country = await countries.find(
 			(element) => element.numericCode === numericCode
 		);
@@ -40,15 +39,40 @@ function Detail() {
 			});
 	}, []);
 
-	if (!isLoading) {
-		console.log(country.name);
-	}
-
 	return (
 		<main className='bg-gray-100 h-screen flex flex-col items-center pb-12 px-6  shadow-inner'>
 			<div className='container mx-auto'>
 				<ButtonBack />
-				<img src='' alt='' />
+
+				{isLoading ? (
+					<h1>Loaded</h1>
+				) : (
+					<section>
+						<div>
+							<img src={country.flag} alt='flag country' />
+						</div>
+						<div>
+							<h1>{country.name}</h1>
+							<div>
+								<ul>
+									<li>Native Name: </li>
+									<li>Population: </li>
+									<li>Region: </li>
+									<li>Sub Region: </li>
+									<li>Capital: </li>
+								</ul>
+								<ul>
+									<li>Top Level Domain:</li>
+									<li>Currencies:</li>
+									<li>Languages:</li>
+								</ul>
+							</div>
+							<div>
+								<p>Border Counties</p>
+							</div>
+						</div>
+					</section>
+				)}
 			</div>
 		</main>
 	);

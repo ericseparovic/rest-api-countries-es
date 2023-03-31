d0d54147b9d9077394464c2e35650575d4078631
Eric Separovic <ericseparovic07@gmail.com>
2023-03-31 17:43:37 -0300

    Add loader

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,6 +1,7 @@
 import ButtonBack from '../components/ButtonBack';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import '../loading.css';
 
 function Detail() {
 	// Get code numeric of url
@@ -31,58 +32,54 @@ function Detail() {
 		? new Intl.NumberFormat().format(country.population)
 		: 'N/A';
 
-	const flag = country ? country.flag : country;
-	console.log(flag);
-
 	return (
 		<main className='bg-gray-100 h-full flex flex-col items-center pb-12 px-6  shadow-inner'>
 			<div className='container mx-auto'>
 				<ButtonBack />
-				<h1>{}</h1>
 
-				{country ? (
+				{!isLoading ? (
 					<section className='my-12 flex sm:flex-row flex-col'>
 						<div>
-							<img src={country.flag} alt='flag country' />
+							<img src={country?.flag} alt='flag country' />
 						</div>
 						<div className='mt-12'>
-							<h1 className='font-bold text-xl mb-4'>{country.name}</h1>
+							<h1 className='font-bold text-xl mb-4'>{country?.name}</h1>
 							<div>
 								<ul className='mb-10'>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
-										Native Name: <span>{country.nativeName}</span>
+										Native Name: <span>{country?.nativeName}</span>
 									</li>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
 										Population: <span>{populationFormat}</span>{' '}
 									</li>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
-										Region: <span>{country.region}</span>{' '}
+										Region: <span>{country?.region}</span>{' '}
 									</li>
 
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
-										Sub Region: <span>{country.subregion}</span>{' '}
+										Sub Region: <span>{country?.subregion}</span>{' '}
 									</li>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
-										Capital: <span>{country.capital}</span>{' '}
+										Capital: <span>{country?.capital}</span>{' '}
 									</li>
 								</ul>
 								<ul className='mb-10'>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
 										Top Level Domain:
-										{country.topLevelDomain.map((domain) => {
+										{country?.topLevelDomain.map((domain) => {
 											return <span key={domain}>{domain}</span>;
 										})}
 									</li>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
 										Currencies:{' '}
-										{country.currencies &&
+										{country?.currencies &&
 											country.currencies.map((currency) => {
 												return <span key={currency.code}>{currency.name}</span>;
 											})}
 									</li>
 									<li className='text-sm pb-2 text-gray-800 font-medium'>
 										Languages:{' '}
-										{country.languages.map((language) => {
+										{country?.languages.map((language) => {
 											return (
 												<span key={language.iso639_1}>{language.name}</span>
 											);
@@ -93,25 +90,36 @@ function Detail() {
 							<div>
 								<h4 className='text-md font-medium mb-4'>Border Counties:</h4>
 								<div className='grid grid-cols-3 gap-2'>
-									{country.borders ? (
+									{country && country.borders ? (
 										country.borders.map((border) => {
 											return (
 												<div
 													key={border}
-													className='shadow-lg w-24 py-1  bg-white rounded-sm text-gray-700 flex items-center justify-center gap-2'>
+													className='shadow-lg w-24 py-1 bg-white rounded-sm text-gray-700 flex items-center justify-center gap-2'>
 													<p>{border}</p>
 												</div>
 											);
 										})
 									) : (
-										<p>Has no borders</p>
+										<h1>hola</h1>
 									)}
 								</div>
 							</div>
 						</div>
 					</section>
 				) : (
-					<h1>Loaded</h1>
+					<div className='flex justify-center items-center mt-36'>
+						<div className='lds-roller'>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
+					</div>
 				)}
 			</div>
 		</main>
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,6 +2,7 @@ import Search from '../components/Search';
 import Filter from '../components/Filter';
 import Country from '../components/Country';
 import { useState, useEffect } from 'react';
+import '../loading.css';
 
 function Main() {
 	const [countries, setCountries] = useState([]);
@@ -22,23 +23,38 @@ function Main() {
 	}, []);
 
 	return (
-		<main className='bg-gray-100 h-full flex flex-col items-center pb-12 px-6  shadow-inner'>
-			<div className='container mx-auto'>
+		<main className='bg-gray-100 h-full flex flex-col items-center pb-12 px-6  shadow-inner min-h-screen'>
+			<div className='container mx-auto w-full'>
 				<form className='md:flex justify-between mt-8'>
 					<Search />
 					<Filter />
 				</form>
-				<section className='grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-12 place-items-center w-full'>
-					{countries.map((country) => {
-						return (
-							<Country
-								country={country}
-								key={country.numericCode}
-								numericCode={country.numericCode}
-							/>
-						);
-					})}
-				</section>
+				{!isLoading ? (
+					<section className='grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-12 place-items-center w-full'>
+						{countries.map((country) => {
+							return (
+								<Country
+									country={country}
+									key={country.numericCode}
+									numericCode={country.numericCode}
+								/>
+							);
+						})}
+					</section>
+				) : (
+					<div className='flex justify-center items-center mt-36'>
+						<div className='lds-roller'>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
+					</div>
+				)}
 			</div>
 		</main>
 	);
